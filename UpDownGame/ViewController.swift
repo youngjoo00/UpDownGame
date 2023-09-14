8e1c74d9c78feb569f7e42aaff37c29f9e5e7281
youngjoo00 <yungjoo0624@gmail.com>
2023-09-15 02:30:25 +0900

    feat: slider, Hit button 추가

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // ui를 값으로 받아오기 위해 사용하는 아웃렛 변수
+    @IBOutlet weak var slider: UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print("현재 값 : \(sender.value)")
+    }
+    
+    
+    @IBAction func touchUpHitButton(_ sender: UIButton) {
+        // 변수로 만든 slider 사용
+        print(slider.value)
+    }
+    
 
 }
 
